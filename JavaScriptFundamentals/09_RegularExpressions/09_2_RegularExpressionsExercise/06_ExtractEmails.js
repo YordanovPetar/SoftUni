737f2981fd2c408d9b0ece36a85cdcfb6b379fe2
Petar Yordanov <yordanov.petar@hotmail.com>
2023-04-01 16:52:06 +0300

    Regular Expression Exercises tasks completed

---
+++ JavaScriptFundamentals/09_RegularExpressions/09_2_RegularExpressionsExercise/06_ExtractEmails.js
@@ -0,0 +1,9 @@
+function extractEmail(text) {
+    let pattern = /(?<=\s|^)([A-Za-z0-9]+[A-Za-z0-9.\-_]*)@[a-z]+\-?[a-z]+(\.[a-z]+)+/g;
+    let emails = text.match(pattern);
+    for (let email of emails) {
+        console.log(email);
+    }
+}
+
+extractEmail('Just send email to s.miller@mit.edu and j.hopking@york.ac.uk for more information.');
